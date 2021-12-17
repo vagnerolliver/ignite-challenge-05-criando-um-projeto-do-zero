1495479ad4753990aa75bfe0612aa3bc97042226
Vagner Oliveira <vagner.olliver@yahoo.com.br>
2021-12-17 08:36:29 -0300

    feat: page posts list

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,18 @@
-export default function Header() {
-  // TODO
+import Image from 'next/image';
+
+import styles from './header.module.scss';
+
+export default function Header(): JSX.Element {
+  return (
+    <div className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <Image
+          src="/images/Logo.svg"
+          alt="Título do Blog"
+          width="239"
+          height="27"
+        />
+      </div>
+    </div>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,10 @@
+import { FiUser, FiArchive } from 'react-icons/fi';
 import { GetStaticProps } from 'next';
+import { useState } from 'react';
 
 import { getPrismicClient } from '../services/prismic';
+import { formatDatePtBR } from '../helpers/datePtBR';
+import Header from '../components/Header';
 
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
@@ -24,13 +28,62 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home({
+  next_page,
+  results,
+}: PostPagination): JSX.Element {
+  const [posts, setPosts] = useState<Post[]>(results);
+  const [nextPage, setNextPage] = useState(next_page);
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
+  function loadMorePosts(): void {
+    fetch(next_page)
+      .then(response => response.json())
+      .then((data: PostPagination) => {
+        setPosts([...posts, ...data.results]);
+        setNextPage(data.next_page);
+      });
+  }
 
-//   // TODO
-// };
+  return (
+    <>
+      <Header />
+      <div className={commonStyles.container}>
+        <div className={styles.HomeContent}>
+          {posts.map(post => (
+            <a key={post.uid}>
+              <strong>{post.data.title}</strong>
+              <p>{post.data.subtitle}</p>
+              <span>
+                <FiUser />
+                {formatDatePtBR(new Date(post.first_publication_date))}
+                <FiArchive /> {post.data.author}
+              </span>
+            </a>
+          ))}
+        </div>
+
+        {nextPage && (
+          <div className={styles.HomeFooter}>
+            <button type="button" onClick={loadMorePosts}>
+              Carregar mais posts
+            </button>
+          </div>
+        )}
+      </div>
+    </>
+  );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+  const postsResponse = await prismic.query('', {
+    pageSize: 1,
+  });
+
+  return {
+    props: {
+      results: postsResponse.results,
+      next_page: postsResponse.next_page,
+    },
+  };
+};
