a76685877ed41a02e4ffc027bdf623aab48c151d
Vagner Oliveira <vagner.olliver@yahoo.com.br>
2021-12-22 08:13:06 -0300

    refactor: header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,18 +1,22 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
+import commonStyles from '../../styles/common.module.scss';
 import styles from './header.module.scss';
 
 export default function Header(): JSX.Element {
   return (
-    <div className={styles.headerContainer}>
-      <div className={styles.headerContent}>
-        <Image
-          src="/images/Logo.svg"
-          alt="Título do Blog"
-          width="239"
-          height="27"
-        />
-      </div>
-    </div>
+    <header className={`${commonStyles.container} ${styles.header}`}>
+      <Link href="/">
+        <a>
+          <Image
+            src="/images/Logo.svg"
+            alt="Título do Blog"
+            width="239"
+            height="27"
+          />
+        </a>
+      </Link>
+    </header>
   );
 }
